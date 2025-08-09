883570bf9998c0c6b237194129c3c0559c14184d
unknown <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-08-09 20:49:26 +0800

    feat: add sidebar animations

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,18 +1,32 @@
-import './home.css'
+"use client";
+
+import './home.css';
 import Sidebar from "../../component/sections/Sidebar";
 import Topbar from "../../component/sections/Topbar2";
+import { useState } from 'react';
 
 export default function HomePage() {
+    const [sidebarOpen, setSidebarOpen] = useState(true);
+
     return (
-        <div className="home-background min-h-screen w-full flex flex-row">
-            <Sidebar />
-            <div className="flex-1 flex flex-col">
-                <Topbar />
+        <div className="home-background min-h-screen w-full flex">
+            {/* Sidebar column with animated width */}
+            <div
+                className={`${sidebarOpen ? 'w-64' : 'w-0'} transition-[width] duration-200 ease-in-out overflow-hidden`}
+            >
+                <Sidebar />
+            </div>
+
+            {/* Content column grows to fill */}
+            <div className="flex-1 flex flex-col min-w-0">
+                <Topbar
+                    onToggleSidebar={() => setSidebarOpen((v) => !v)}
+                    isSidebarOpen={sidebarOpen}
+                />
 
                 <div className="flex-1 p-8">
                     main
                 </div>
-                
             </div>
         </div>
     );
--- src/component/sections/Topbar2.tsx
+++ src/component/sections/Topbar2.tsx
@@ -1,20 +1,33 @@
+"use client";
+
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-export default function Topbar() {
-    return(
+type TopbarProps = {
+    onToggleSidebar?: () => void;
+    isSidebarOpen?: boolean;
+};
+
+export default function Topbar({ onToggleSidebar, isSidebarOpen }: TopbarProps) {
+    return (
         <div className="bg-gradient-to-r from-[#F8F2F2] via-[#fff] to-[#F8F2F2] w-full p-5 shadow-md outline outline-black/10">
             <div className="flex items-center justify-between">
-                <button type="button" aria-label="Open sidebar" className="text-[#737373] hover:text-[#800000] transition-colors">
+                <button
+                    type="button"
+                    aria-label={isSidebarOpen ? 'Hide sidebar' : 'Show sidebar'}
+                    aria-pressed={isSidebarOpen}
+                    onClick={onToggleSidebar}
+                    className="text-[#737373] hover:text-[#800000] transition-colors"
+                >
                     <MenuOutlinedIcon />
                 </button>
 
                 <button
                     type="button"
                     aria-label="Open profile menu"
-                    className="items-center justify-center text-[#737373] mr-5"
+                    className="inline-flex items-center justify-center text-[#737373]"
                 >
-                    <AccountCircleIcon className="m-0 p-0 leading-none scale-160" />
+                    <AccountCircleIcon className="m-0 p-0 leading-none scale-125" />
                 </button>
             </div>
         </div>
